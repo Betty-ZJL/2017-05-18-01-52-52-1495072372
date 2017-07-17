55806cdf928fd5882b838d189b70cfe0eee42906
Betty-ZJL <Betty-ZJL@users.noreply.github.com>
2017-07-17 17:19:58 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,18 +1,41 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.length}
+o) 平均值 = ${sequence.average()}
 `);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+      this.length=input.length;
+      for (let i=0;i<this.length;i++){
+          this[i]=input[i];
+      }
   }
 
   minimum() {
-    // Write your code here
+      let min=this[0];
+      for(let i=1;i<this.length;i++)
+          if(min>this[i])
+              min=this[i];
+      return min; 
   }
-
-  // Write your code here
+  
+  maximum() {
+      let max=this[0];
+      for(let i=1;i<this.length;i++)
+          if(max<this[i])
+              max=this[i];
+      return max; 
+  }
+  
+  average(){
+      let ave=0;
+      for(let i=0;i<this.length;i++)
+          ave=ave+this[i];
+      ave=ave/this.length;
+      return ave.toFixed(2);
+  }
 }
